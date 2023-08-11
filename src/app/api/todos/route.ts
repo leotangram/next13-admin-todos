e7d9991785168a8c032093cb12b74731b16d38e1
Leonardo Omaña <leotangram@gmail.com>
2023-08-11 14:53:29 -0500

    simple pagination

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,7 +2,19 @@ import prisma from '@/lib/prisma'
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(request: Request) {
-  const todos = await prisma.todo.findMany()
+  const { searchParams } = new URL(request.url)
+  const skip = Number(searchParams.get('skip') ?? '0')
+  const take = Number(searchParams.get('take') ?? '10')
+
+  if (isNaN(skip))
+    return NextResponse.json({ error: 'Invalid "skip" param' }, { status: 400 })
+  if (isNaN(take))
+    return NextResponse.json({ error: 'Invalid "take" param' }, { status: 400 })
+
+  const todos = await prisma.todo.findMany({
+    skip,
+    take
+  })
 
   return NextResponse.json(todos)
 }
