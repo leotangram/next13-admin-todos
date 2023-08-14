2fa800f7e517d35a38765bef0619b9a4bfc89477
Leonardo Omaña <leotangram@gmail.com>
2023-08-14 17:18:56 -0500

    update put entries

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -19,3 +19,22 @@ export async function GET(request: Request, { params }: Segments) {
 
   return NextResponse.json(todo)
 }
+
+export async function PUT(request: Request, { params }: Segments) {
+  const { id } = params
+  const todo = await prisma.todo.findFirst({ where: { id } })
+
+  if (!todo)
+    return NextResponse.json(
+      { message: `Todo with id ${id} not found}` },
+      { status: 404 }
+    )
+
+  const body = await request.json()
+  const updatedTodo = await prisma.todo.update({
+    where: { id },
+    data: body
+  })
+
+  return NextResponse.json(updatedTodo)
+}
