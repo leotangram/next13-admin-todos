e3dd0d7375122a2e2bc4ba5ca51ec45948338dfa
Leonardo Omaña <leotangram@gmail.com>
2023-08-14 17:00:27 -0500

    POST - create new entry

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,21 @@
+import prisma from '@/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
+
+interface Segments {
+  params: {
+    id: string
+  }
+}
+
+export async function GET(request: Request, { params }: Segments) {
+  const { id } = params
+  const todo = await prisma.todo.findFirst({ where: { id } })
+
+  if (!todo)
+    return NextResponse.json(
+      { message: `Todo with id ${id} not found}` },
+      { status: 404 }
+    )
+
+  return NextResponse.json(todo)
+}
