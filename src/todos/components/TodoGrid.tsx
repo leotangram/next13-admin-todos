b1c9d2f981d9ef664098913127b6d2ef2757e4a2
Leonardo Omaña <leotangram@gmail.com>
2023-08-18 15:24:27 -0500

    update ToDo

--- src/todos/components/TodoGrid.tsx
+++ src/todos/components/TodoGrid.tsx
@@ -3,16 +3,26 @@
 import { Todo } from '@prisma/client'
 import { FC } from 'react'
 import { TodoItem } from './TodoItem'
+import * as todosApi from '@/todos/helpers/todos'
+import { useRouter } from 'next/navigation'
 
 interface TodoGridProps {
   todos?: Todo[]
 }
 
 export const TodoGrid: FC<TodoGridProps> = ({ todos = [] }) => {
+  const router = useRouter()
+
+  const toggleTodo = async (id: string, complete: boolean) => {
+    await todosApi.updateTodo(id, complete)
+
+    router.refresh()
+  }
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
       {todos.map(todo => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} togleTodo={toggleTodo} />
       ))}
     </div>
   )
--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { FC } from 'react'
 import { Todo } from '@prisma/client'
 
@@ -6,16 +8,17 @@ import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5'
 
 interface TodoItemProps {
   todo: Todo
-  // TODO: Acciones que quiero llamar
+  togleTodo: (id: string, complete: boolean) => Promise<Todo | void>
 }
 
-export const TodoItem: FC<TodoItemProps> = ({ todo }) => {
-  const { complete, description } = todo
+export const TodoItem: FC<TodoItemProps> = ({ todo, togleTodo }) => {
+  const { id, complete, description } = todo
 
   return (
     <div className={todo.complete ? styles.todoDone : styles.todoPending}>
       <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
         <div
+          onClick={() => togleTodo(id, !complete)}
           className={`flex p-2 rounded-md cursor-pointer hover:bg-opacity-60 ${
             complete ? 'bg-blue-100' : 'bg-red-100'
           }`}
