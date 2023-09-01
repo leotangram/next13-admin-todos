abdb4dfdeea6d5ad334d5f50d3c3d7754db79557
Leonardo Omaña <leotangram@gmail.com>
2023-08-31 19:49:58 -0500

    login logout

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -13,6 +13,7 @@ import { SidebarItem } from './SidebarItem'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import { redirect } from 'next/navigation'
+import { LogoutButton } from './LogoutButton'
 
 const menuItems = [
   {
@@ -96,10 +97,7 @@ export const Sidebar = async () => {
       </div>
 
       <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-          <CiLogout />
-          <span className="group-hover:text-gray-700">Logout</span>
-        </button>
+        <LogoutButton />
       </div>
     </aside>
   )
